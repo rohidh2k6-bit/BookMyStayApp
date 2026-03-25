2efe11caef2af8144c8da9e45bc917fff66a09b2
bhavya-code <cherukubhavayasree@email.com>
2026-03-25 09:19:24 +0530

    Added UC2-RoomTypes

--- Book/src/BookMyStay.java
+++ Book/src/BookMyStay.java
@@ -1,6 +1,46 @@
+abstract class Room {
+    String type;
+    int price;
+
+    Room(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println(type + " Room - Price: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() { super("Single", 1000); }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() { super("Double", 2000); }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() { super("Suite", 5000); }
+}
+
 public class BookMyStay {
     public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay App");
-        System.out.println("Version: 1.0");
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        r1.display();
+        System.out.println("Available: " + singleAvailable);
+
+        r2.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        r3.display();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
