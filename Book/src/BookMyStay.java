cb65a802fda5a5bb472245d389b7c93c1de85e9a
bhavya-code <cherukubhavayasree@email.com>
2026-03-25 10:14:52 +0530

    Added UC10-Cancellation

--- Book/src/BookMyStay.java
+++ Book/src/BookMyStay.java
@@ -1,32 +1,21 @@
 import java.util.*;
 
-class InvalidBookingException extends Exception {
-    InvalidBookingException(String msg) {
-        super(msg);
-    }
-}
-
 public class BookMyStay {
     public static void main(String[] args) {
 
         Map<String, Integer> inventory = new HashMap<>();
-        inventory.put("Single", 1);
+        inventory.put("Single", 0);
 
-        String request = "Suite";
+        Stack<String> rollback = new Stack<>();
+        rollback.push("S-101");
 
-        try {
-            if (!inventory.containsKey(request)) {
-                throw new InvalidBookingException("Invalid Room Type");
-            }
+        System.out.println("Cancelling booking...");
 
-            if (inventory.get(request) <= 0) {
-                throw new InvalidBookingException("No Rooms Available");
-            }
+        inventory.put("Single", inventory.get("Single") + 1);
 
-            System.out.println("Booking Success");
+        String released = rollback.pop();
 
-        } catch (InvalidBookingException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        System.out.println("Room Released: " + released);
+        System.out.println("Updated Inventory: " + inventory);
     }
 }
