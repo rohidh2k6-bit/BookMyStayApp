a9b97cdd3bdafdbabb5245bfa99306d2ca893d92
bhavya-code <cherukubhavayasree@email.com>
2026-03-25 09:22:16 +0530

    Added UC3-Inventory

--- Book/src/BookMyStay.java
+++ Book/src/BookMyStay.java
@@ -1,46 +1,22 @@
-abstract class Room {
-    String type;
-    int price;
+import java.util.HashMap;
 
-    Room(String type, int price) {
-        this.type = type;
-        this.price = price;
+class RoomInventory {
+    HashMap<String, Integer> inventory = new HashMap<>();
+
+    RoomInventory() {
+        inventory.put("Single", 5);
+        inventory.put("Double", 3);
+        inventory.put("Suite", 2);
     }
 
     void display() {
-        System.out.println(type + " Room - Price: " + price);
+        System.out.println(inventory);
     }
-}
-
-class SingleRoom extends Room {
-    SingleRoom() { super("Single", 1000); }
-}
-
-class DoubleRoom extends Room {
-    DoubleRoom() { super("Double", 2000); }
-}
-
-class SuiteRoom extends Room {
-    SuiteRoom() { super("Suite", 5000); }
 }
 
 public class BookMyStay {
     public static void main(String[] args) {
-        Room r1 = new SingleRoom();
-        Room r2 = new DoubleRoom();
-        Room r3 = new SuiteRoom();
-
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
-
-        r1.display();
-        System.out.println("Available: " + singleAvailable);
-
-        r2.display();
-        System.out.println("Available: " + doubleAvailable);
-
-        r3.display();
-        System.out.println("Available: " + suiteAvailable);
+        RoomInventory inv = new RoomInventory();
+        inv.display();
     }
 }
