6d8642361653c7cba932bc4d3834f36e785d7a70
bhavya-code <cherukubhavayasree@email.com>
2026-03-25 09:16:51 +0530

    Added UC1-WelcomePage

---
+++ Book/src/BookMyStay.java
@@ -0,0 +1,6 @@
+public class BookMyStay {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Version: 1.0");
+    }
+}
